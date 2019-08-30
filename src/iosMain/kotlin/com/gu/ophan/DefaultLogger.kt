4c55d87d04ff8a92207b0b1d3a9d5ffd3a8ab3e2
Max Spencer <mrmaxspencer@googlemail.com>
2019-08-30 11:25:16 +0100

    Stash: Made iOS impl

---
+++ src/iosMain/kotlin/com/gu/ophan/DefaultLogger.kt
@@ -0,0 +1,11 @@
+package com.gu.ophan
+
+actual class DefaultLogger : Logger {
+    override fun debug(tag: String, message: String) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun warn(tag: String, message: String, error: Throwable?) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+}
